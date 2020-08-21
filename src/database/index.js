d565d301e1ed0ab5bd09e2848c146e986a7e05e5
Rafael-PC <rafael_rx@hotmail.com>
2020-08-21 14:25:31 -0300

    Add follow a users followers

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,11 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
 
-mongoose.connect(CONNECTION_STRING, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useCreateIndex: true, 
+mongoose.connect(CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 mongoose.Promise = global.Promise;
--- src/instagram.js
+++ src/instagram.js
@@ -1,115 +1,158 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const unfollowUser = require("./model/unfollowUser");
 
-const BASE_URL = 'https://instagram.com/';
+const BASE_URL = "https://instagram.com/";
 const TAG_URL = (tag) => `https://www.instagram.com/explore/tags/${tag}/`;
 const USER_URL = (user) => `https://www.instagram.com/${user}/`;
 
 const instagram = {
-    browser: null,
-    page: null,
+  browser: null,
+  page: null,
 
-    initialize: async () => {
-        instagram.browser = await puppeteer.launch({
-            headless: false
-        });
+  initialize: async () => {
+    instagram.browser = await puppeteer.launch({
+      headless: false,
+    });
 
-        instagram.page = await instagram.browser.newPage();
-    },
+    instagram.page = await instagram.browser.newPage();
+  },
 
-    login: async (username, password) => {       
-        await instagram.page.goto(BASE_URL, { waitUntil: 'networkidle2' });
+  login: async (username, password) => {
+    console.log(`[instagram-bot] Logando na conta: ${username}`);
+    await instagram.page.goto(BASE_URL, { waitUntil: "networkidle2" });
 
+    await instagram.page.waitFor(1000);
+
+    await instagram.page.type('input[name="username"]', username, {
+      delay: 50,
+    });
+    await instagram.page.type('input[name="password"]', password, {
+      delay: 50,
+    });
+
+    let loginButton = await instagram.page.$x(
+      '//div[contains(text(), "Entrar")]'
+    );
+
+    await loginButton[0].click();
+
+    await instagram.page.waitFor(5000);
+
+    console.log(`[instagram-bot] Login realizado!`);
+  },
+  likeAndFollowTagsProccess: async (tags = []) => {
+    for (let tag of tags) {
+      await instagram.page.goto(TAG_URL(tag), { waitUntil: "networkidle2" });
+      await instagram.page.waitFor(1000);
+
+      let posts = await instagram.page.$$(
+        'article > div:nth-child(3) img[decoding="auto"]'
+      );
+
+      for (let i = 0; i < process.env.NUMBER_LIKES; i++) {
+        let post = posts[i];
+
+        await post.click();
+
+        await instagram.page.waitFor('div[id="react-root"]');
         await instagram.page.waitFor(1000);
 
-        await instagram.page.type('input[name="username"]', username, {delay: 50});
-        await instagram.page.type('input[name="password"]', password, {delay: 50});
+        const names = await instagram.page.$$("div.e1e1d > a");
+        const name = await instagram.page.evaluate(
+          (names) => names.textContent,
+          names[0]
+        );
+        instagram.saveUserInDB(name);
+        if (name) {
+          let isFollowed = await instagram.page.$x(
+            '//button[contains(text(), "Seguir")]'
+          );
+          let isLikeble = await instagram.page.$('svg[aria-label="Curtir"]');
 
-        let loginButton = await instagram.page.$x('//div[contains(text(), "Entrar")]');
+          await instagram.page.waitFor(1000);
 
-        await loginButton[0].click();
+          if (isFollowed) {
+            await isFollowed[1].click('//button[contains(text(), "Seguir")]');
+          }
 
-        await instagram.page.waitFor(10000);
-    },    
-    likeAndFollowTagsProccess: async (tags = []) => {
-        for(let tag of tags){
-            await instagram.page.goto(TAG_URL(tag), { waitUntil: 'networkidle2' });
-            await instagram.page.waitFor(1000);
+          await instagram.page.waitFor(1000);
 
-            let posts = await instagram.page.$$('article > div:nth-child(3) img[decoding="auto"]');
-
-            for(let i =0; i < process.env.NUMBER_LIKES; i++){
-                let post = posts[i];
-
-                await post.click();
-                
-                await instagram.page.waitFor('div[id="react-root"]');
-                await instagram.page.waitFor(1000);
-
-                const names = await instagram.page.$$('div.e1e1d > a');
-                const name = await instagram.page.evaluate(names => names.textContent, names[0]);
-                instagram.saveUserInDB(name);
-                if(name){
-                    let isFollowed = await instagram.page.$x('//button[contains(text(), "Seguir")]');
-                    let isLikeble = await instagram.page.$('svg[aria-label="Curtir"]');
-                    
-                    await instagram.page.waitFor(1000);
-
-                    if(isFollowed){
-                        await isFollowed[1].click('//button[contains(text(), "Seguir")]');
-                    }
-
-                    await instagram.page.waitFor(1000);
-
-                    if(isLikeble){
-                        await instagram.page.click('svg[aria-label="Curtir"]');
-                    }               
-                }                
-                
-                await instagram.page.waitFor(2000);
-
-                await instagram.page.click('svg[aria-label="Fechar"]');
-                
-                await instagram.page.waitFor(2000);
-            }
-            await instagram.page.waitFor(5000);
+          if (isLikeble) {
+            await instagram.page.click('svg[aria-label="Curtir"]');
+          }
         }
-    },
-    unfollowUser: async () => {
-        const usernames = await instagram.findUserInDB();
-        for(let username of usernames){            
-            await instagram.page.goto(USER_URL(username), { waitUntil: 'networkidle2' });
-            await instagram.page.waitFor(1000);      
-            
-            let isFollowed = await instagram.page.$('span[aria-label="Seguindo"]');         
-            if(isFollowed){
-                await isFollowed[1].click('span[aria-label="Seguindo"]');
-                await instagram.page.waitFor(1000); 
-                await instagram.page.$x('//button[contains(text(), "Deixar de seguir")]')[0].click();
-            }   
-            await instagram.page.waitFor(3000);  
-            instagram.deleteUserInDB(username);
-        }
-    },
-    saveUserInDB: async (user) => {        
-        await unfollowUser.create({name: user});
-    },
-    findUserInDB: async () => {   
-        let usernames = [];     
-        await unfollowUser.find({}, function(err, docs) {
-            if (!err) { 
-                for(let doc in docs){
-                    usernames.push(docs[doc].name);
-                }
-                              
-            }
-        });
-        console.log(usernames);
-        return usernames;
-    },
-    deleteUserInDB: async (user) => {        
-        await unfollowUser.findOneAndDelete({name: user});
+
+        await instagram.page.waitFor(2000);
+
+        await instagram.page.click('svg[aria-label="Fechar"]');
+
+        await instagram.page.waitFor(2000);
+      }
+      await instagram.page.waitFor(5000);
     }
-}
+  },
+  followUsersFollowers: async (users = []) => {
+    for (let user of users) {
+      console.log(`[instagram-bot] Acessando perfil do usuário: ${user}`);
+      await instagram.page.goto(USER_URL(user), { waitUntil: "networkidle2" });
+      await instagram.page.waitFor(1000);
+      const listFollowers = await instagram.page.$$(
+        "#react-root > section > main > div > header > section > ul > li:nth-child(2) > a"
+      );
+
+      console.log(`[instagram-bot] Abrindo seguidores!`);
+      listFollowers[0].click();
+      await instagram.page.waitFor(3000);
+
+      console.log(`[instagram-bot] Seguindo!`);
+      await instagram.page.evaluate(() => {
+        let elements = document.getElementsByClassName("sqdOP");
+        for (let element of elements) element.click();
+      });
+
+      await instagram.page.waitFor(3000);
+    }
+  },
+  unfollowUser: async () => {
+    const usernames = await instagram.findUserInDB();
+    for (let username of usernames) {
+      await instagram.page.goto(USER_URL(username), {
+        waitUntil: "networkidle2",
+      });
+      await instagram.page.waitFor(1000);
+
+      let isFollowed = await instagram.page.$('span[aria-label="Seguindo"]');
+      if (isFollowed) {
+        await isFollowed[1].click('span[aria-label="Seguindo"]');
+        await instagram.page.waitFor(1000);
+        await instagram.page
+          .$x('//button[contains(text(), "Deixar de seguir")]')[0]
+          .click();
+      }
+      await instagram.page.waitFor(3000);
+      instagram.deleteUserInDB(username);
+    }
+  },
+  saveUserInDB: async (user) => {
+    await unfollowUser.create({ name: user });
+  },
+  findUserInDB: async () => {
+    let usernames = [];
+    await unfollowUser.find({}, function (err, docs) {
+      if (!err) {
+        for (let doc in docs) {
+          usernames.push(docs[doc].name);
+        }
+      }
+    });
+    return usernames;
+  },
+  deleteUserInDB: async (user) => {
+    await unfollowUser.findOneAndDelete({ name: user });
+  },
+  close: async () => {
+    await instagram.browser.close();
+  },
+};
 
 module.exports = instagram;
